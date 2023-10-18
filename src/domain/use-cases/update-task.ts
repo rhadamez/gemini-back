5a5a2ee3b511419d1fd45e39fd7a6799739cca8f
Rhadamez Gindri Hercilio <rhadamez@gmail.com>
2023-10-18 10:45:52 -0300

    feat: add update-task use-case

--- src/domain/use-cases/update-task.ts
+++ src/domain/use-cases/update-task.ts
@@ -1,5 +1,6 @@
 import { Task } from '@/domain/entities/Task';
 import { TaskRepository } from '../contracts/task-repository';
+import { TaskNotFound } from '../entities/errors/task-not-found';
 
 interface Input {
   id: number;
@@ -10,5 +11,16 @@ interface Input {
 export class UpdateTask {
   constructor(private taskRepository: TaskRepository) {}
 
-  async execute({ id, description, done }: Input): Promise<Task> {}
+  async execute({ id, description, done }: Input): Promise<Task> {
+    const taskExists = await this.taskRepository.findById(id);
+
+    if (!taskExists) throw new TaskNotFound();
+
+    if (description) taskExists.description = description;
+    if (done) taskExists.done = done;
+
+    const taskCreated = await this.taskRepository.update(taskExists);
+
+    return taskCreated;
+  }
 }
